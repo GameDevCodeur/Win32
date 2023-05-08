255d9ead766e0e4f118da9c67d2f6f10aa0d826d
Patrick MAES <98809379+GameDevCodeur@users.noreply.github.com>
2023-05-08 03:49:42 +0200

    Update build.bat

--- 000/build.bat
+++ 000/build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET compilerFlags=-pedantic-errors -Wall -Werror -O3 -std=c17 -Wextra -Wconversion -m64 -mwindows
-
+ECHO "Building %assembly%%..."
 clang %compilerFlags% code/000.c -o release/000.exe
 
 pause
